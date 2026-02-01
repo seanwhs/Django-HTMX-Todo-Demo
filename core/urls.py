583b409506e7db713f4b9c618c3f9d9e3a702484
unknown <you@example.com>
2026-02-02 07:47:02 +0800

    added inline edit

---
+++ core/urls.py
@@ -0,0 +1,16 @@
+# core/urls.py
+from django.contrib import admin
+from django.urls import path
+from todo import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.todos, name='todos'),
+    path('filter/<str:filter_type>/', views.todos, name='filter_todos'),
+    path('add-todo/', views.add_todo, name='add_todo'),
+    path('update-todo/<int:pk>/', views.update_todo, name='update_todo'),
+    path('delete-todo/<int:pk>/', views.delete_todo, name='delete_todo'),
+    path('toggle-all/', views.toggle_all, name='toggle_all'),
+    path('clear-completed/', views.clear_completed, name='clear_completed'),
+    path('edit-todo/<int:pk>/', views.edit_todo, name='edit_todo'),
+]
